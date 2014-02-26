3ba90de8b935fddf3c94f3b912f13b6e403c1038
Marco Lau <lau.career@gmail.com>
2014-02-26 16:43:26 +0800

    Added someoneWon and announceWinner.

--- app/js/application.coffee
+++ app/js/application.coffee
@@ -26,13 +26,20 @@ class BoardCtrl
     c2 = c[pattern[2]] || pattern[2]
     "#{c0}#{c1}#{c2}"
 
-  checkForWin: (board) ->
+  someoneWon: (board) ->
     'xxx' == board || 'ooo' == board
+
+  resetBoard: =>
+    @$scope.cells = {}
+
+  announceWinner: =>
+    winner = if Object.keys(@$scope.cells).length % 2 == 0 then 'o' else 'x'
+    alert "#{winner} wins!"
 
   parseBoard: =>
     for pattern in @Settings.WIN_PATTERNS
       board = @getBoard(pattern)
-      console.log @checkForWin(board)
+      @announceWinner() if @someoneWon(board)
 
   mark: (@$event) =>
     cell = @$event.target.dataset.index
--- app/js/application.js
+++ app/js/application.js
@@ -16,6 +16,8 @@
       this.Settings = Settings;
       this.mark = __bind(this.mark, this);
       this.parseBoard = __bind(this.parseBoard, this);
+      this.announceWinner = __bind(this.announceWinner, this);
+      this.resetBoard = __bind(this.resetBoard, this);
       this.getBoard = __bind(this.getBoard, this);
       this.$scope.cells = {};
       this.$scope.mark = this.mark;
@@ -30,8 +32,18 @@
       return "" + c0 + c1 + c2;
     };
 
-    BoardCtrl.prototype.checkForWin = function(board) {
+    BoardCtrl.prototype.someoneWon = function(board) {
       return 'xxx' === board || 'ooo' === board;
+    };
+
+    BoardCtrl.prototype.resetBoard = function() {
+      return this.$scope.cells = {};
+    };
+
+    BoardCtrl.prototype.announceWinner = function() {
+      var winner;
+      winner = Object.keys(this.$scope.cells).length % 2 === 0 ? 'o' : 'x';
+      return alert("" + winner + " wins!");
     };
 
     BoardCtrl.prototype.parseBoard = function() {
@@ -41,7 +53,11 @@
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         pattern = _ref[_i];
         board = this.getBoard(pattern);
-        _results.push(console.log(this.checkForWin(board)));
+        if (this.someoneWon(board)) {
+          _results.push(this.announceWinner());
+        } else {
+          _results.push(void 0);
+        }
       }
       return _results;
     };
