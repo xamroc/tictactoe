dc197b9a8f0ad643188d511692cd21db5bcb70a0
Marco Lau <lau.career@gmail.com>
2014-02-26 16:14:55 +0800

    Added getBoard and parseBoard

--- app/js/application.coffee
+++ app/js/application.coffee
@@ -19,10 +19,25 @@ class BoardCtrl
     @$scope.cells = {}
     @$scope.mark = @mark
 
+  getBoard: (pattern) =>
+    c = @$scope.cells
+    c0 = c[pattern[0]] || pattern[0]
+    c1 = c[pattern[1]] || pattern[1]
+    c2 = c[pattern[2]] || pattern[2]
+    "#{c0}#{c1}#{c2}"
+
+
+  parseBoard: =>
+    console.log '-'
+    for pattern in @Settings.WIN_PATTERNS
+      board = @getBoard(pattern)
+      console.log board
+
   mark: (@$event) =>
     cell = @$event.target.dataset.index
     player = if Object.keys(@$scope.cells).length % 2 == 0 then 'x' else 'o'
     @$scope.cells[cell] = player
+    @parseBoard()
 
 BoardCtrl.$inject = ["$scope", "Settings"]
 ticTacToe.controller "BoardCtrl", BoardCtrl
--- app/js/application.js
+++ app/js/application.js
@@ -15,16 +15,41 @@
       this.$scope = $scope;
       this.Settings = Settings;
       this.mark = __bind(this.mark, this);
+      this.parseBoard = __bind(this.parseBoard, this);
+      this.getBoard = __bind(this.getBoard, this);
       this.$scope.cells = {};
       this.$scope.mark = this.mark;
     }
+
+    BoardCtrl.prototype.getBoard = function(pattern) {
+      var c, c0, c1, c2;
+      c = this.$scope.cells;
+      c0 = c[pattern[0]] || pattern[0];
+      c1 = c[pattern[1]] || pattern[1];
+      c2 = c[pattern[2]] || pattern[2];
+      return "" + c0 + c1 + c2;
+    };
+
+    BoardCtrl.prototype.parseBoard = function() {
+      var board, pattern, _i, _len, _ref, _results;
+      console.log('-');
+      _ref = this.Settings.WIN_PATTERNS;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        pattern = _ref[_i];
+        board = this.getBoard(pattern);
+        _results.push(console.log(board));
+      }
+      return _results;
+    };
 
     BoardCtrl.prototype.mark = function($event) {
       var cell, player;
       this.$event = $event;
       cell = this.$event.target.dataset.index;
       player = Object.keys(this.$scope.cells).length % 2 === 0 ? 'x' : 'o';
-      return this.$scope.cells[cell] = player;
+      this.$scope.cells[cell] = player;
+      return this.parseBoard();
     };
 
     return BoardCtrl;
